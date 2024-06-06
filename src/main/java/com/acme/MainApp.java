66be1a742e03f6651ac8a1df289051661edbf3b5
Fernando Balieiro <fernandobalieiro@gmail.com>
2024-06-06 16:37:11 +0100

    Initial commit.

---
+++ src/main/java/com/acme/MainApp.java
@@ -0,0 +1,23 @@
+package com.acme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * A Camel Application
+ */
+@SpringBootApplication
+public class MainApp {
+
+    /**
+     * A main() so we can easily run these routing rules in our IDE
+     */
+    /**
+     * A main method to start this application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class, args);
+    }
+
+}
+
